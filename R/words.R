7437edaa361b38f9f6f3cb61e85a2f0de27e2087
Donnie Minnick <donnie.minnick@gmail.com>
2019-12-02 21:22:52 -0500

    Enhanced words dataset with additional variables for consonants and vowels.

--- R/words.R
+++ R/words.R
@@ -1,15 +1,19 @@
 
 #' Words Dataset
 #'
-#' A sample set of words taken from the first paragraph of James Joyce's short
-#' story "The Dead" to demonstrate the functionality in this package.
+#' A sample set of words taken from the first paragraph of James Joyce's
+#' short story "The Dead" to demonstrate the functionality in this package.
 #'
-#' The dataset includes the four variables.
+#' The dataset includes the following variables:
 #' \describe{
 #' \item{\code{word}}{Character, word in the sample.}
 #' \item{\code{first_letter}}{Character, first letter of the word.}
 #' \item{\code{last_letter}}{Character, last letter of the word.}
 #' \item{\code{letter_count}}{Integer, number of letters in the word.}
+#' \item{\code{starts_with}}{Character, identifies whether the word starts with
+#' a vowel or consonant.}
+#' \item{\code{ends_with}}{Character, identifies whether the word ends with a
+#' vowel or consonant.}
 #' }
 #'
 #' @docType data
@@ -20,7 +24,7 @@
 #'
 #' @usage data(words)
 #'
-#' @format A data frame with 55 observations and 4 variables.
+#' @format A data frame with 127 observations and 6 variables.
 #'
 #' @author Donnie Minnick \email{donnie.minnick@gmail.com}
 #'
--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -6,65 +6,33 @@ library(stringr)
 
 # Create sequence of words.
 
-words <- c("lily",
-           "the",
-           "caretaker’s",
-           "daughter",
-           "was",
-           "literally",
-           "run",
-           "off",
-           "her",
-           "feet",
-           "hardly",
-           "had",
-           "she",
-           "brought",
-           "one",
-           "gentleman",
-           "into",
-           "the",
-           "little",
-           "pantry",
-           "behind",
-           "the",
-           "office",
-           "on",
-           "the",
-           "ground",
-           "floor",
-           "and",
-           "helped",
-           "him",
-           "off",
-           "with",
-           "his",
-           "overcoat",
-           "than",
-           "the",
-           "wheezy",
-           "hall-door",
-           "bell",
-           "clanged",
-           "again",
-           "and",
-           "she",
-           "had",
-           "to",
-           "scamper",
-           "along",
-           "the",
-           "bare",
-           "hallway",
-           "to",
-           "let",
-           "in",
-           "another",
-           "guest")
+words <- paste("Lily, the caretaker's daughter, was literally run off her feet. ",
+               "Hardly had she brought one gentleman into the little pantry behind ",
+               "the office on the ground floor and helped him off with his overcoat ",
+               "than the wheezy hall-door bell clanged again and she had to scamper ",
+               "along the bare hallway to let in another guest. It was well for her ",
+               "she had not to attend to the ladies also. But Miss Kate and Miss ",
+               "Julia had thought of that and had converted the bathroom upstairs ",
+               "into a ladies' dressing-room. Miss Kate and Miss Julia were there, ",
+               "gossiping and laughing and fussing, walking after each other to the ",
+               "head of the stairs, peering down over the banisters and calling down ",
+               "to Lily to ask her who had come.", sep = "")
+
+# Create a data frame of words.
+
+words <- unlist(strsplit(words, " "))
+
+# Remove punctuation.
+
+words <- gsub("[][,.]", "", words)
+
+# Convert all words in the vector to lower case.
+
+words <- str_to_lower(words)
 
 # Coerce to data frame.
 
-words <- data.frame(words, stringsAsFactors = FALSE)
+words <- as.data.frame(words, stringsAsFactors = FALSE)
 
 # Add column name.
 
@@ -75,7 +43,13 @@ colnames(words) <- c("word")
 words <- words %>%
       mutate(first_letter = str_sub(word, 1, 1),
              last_letter = str_sub(word, str_length(word), str_length(word)),
-             letter_count = str_length(word))
+             letter_count = str_length(word),
+             starts_with = ifelse(first_letter %in% c("a", "e", "i", "o", "u"),
+                                        "vowel",
+                                        "consonant"),
+             ends_with = ifelse(last_letter %in% c("a", "e", "i", "o", "u"),
+                                "vowel",
+                                "consonant"))
 
 # Save data.
 
